c68b2280ca0889bd55ca7326aac935dd5e9a1c11
jihye0526 <samtafe97@naver.com>
2023-01-26 21:17:45 +0900

    4.6 Outlet

---
+++ src/screens/users/Followers.tsx
@@ -0,0 +1,8 @@
+export default function Followers(){
+    return (
+        <>
+            <h3>Followers List</h3>
+            <h3>jihye</h3>
+        </>
+    );
+}
--- src/screens/users/User.tsx
+++ src/screens/users/User.tsx
@@ -1,10 +1,15 @@
-import { useParams } from "react-router-dom";
+import { Link, Outlet, useParams } from "react-router-dom";
 import { users } from "../../db";
 
 export default function User(){
     const { userId } = useParams();
 
     return (
-        <h1>User with it {userId} is name : {users[Number(userId)-1].name}</h1>
+        <div>
+            <h1>User with it {userId} is name : {users[Number(userId)-1].name}</h1>
+            <hr/>
+            <Link to="followers">See Followers</Link> {/* <Link to="/followers">라고 쓰면 localhost:3000/followers로 이동함 */}
+            <Outlet />
+        </div>
     );
 }
